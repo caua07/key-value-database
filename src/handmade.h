220effea3123ced982a13ba4221c0767dc11e8d2
caua07 <ratauzim@gmail.com>
2025-10-10 20:24:16 -0300

    simple file update

--- src/handmade.h
+++ src/handmade.h
@@ -1,33 +1,12 @@
 #pragma once
 #include "./handmade/handmande_threadpool.h"
+#include "./handmade/handmade_errorHandle.h"
+#include <optional>
 #include <iostream>
 #include <shared_mutex>
 #include <unordered_map>
 #include <string>
 
-enum class StatusCode { OK, NotFound, KeyEmpty, ValueEmpty, IOError, ParseError, ServerError };
-
-struct Status {
-  StatusCode code;
-  std::string message;
-
-  bool ok() const { return code == StatusCode::OK; }
-  static Status OK(const std::string& message = "")
-  {
-    return {StatusCode::OK, message};
-  }
-  static Status NotFound(const std::string& key) {
-    return {StatusCode::NotFound, "Key not found: " + key};
-  }
-  static Status KeyEmpty() { return {StatusCode::KeyEmpty, ""};};
-  static Status ValueEmpty(const std::string& key)
-  {
-    return {StatusCode::ValueEmpty, "key holding empty value: " + key};
-  };
-  static Status IOError() { return {StatusCode::IOError, ""};};
-  static Status ParseError() { return {StatusCode::ParseError, ""};};
-  static Status ServerError() { return {StatusCode::ServerError, ""};};
-};
 
 class KeyValueStore {
   protected:
